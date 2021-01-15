cd3635f1b9d052fef9881afe58b83762281ff789
Diego Tardio <diego@Diegos-MacBook-Pro.local>
2021-01-14 23:26:19 -0500

    TESTING]

---
+++ 0x04-sandpiles.c/sandpiles.h
@@ -0,0 +1,11 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+/* HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTYPES */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* _SANDPILES_H_ */
