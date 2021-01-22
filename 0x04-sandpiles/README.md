38bcf40cb870df5ac1e9ebcdf71321dd15e365ad
Diego Tardio <diego@Diegos-MacBook-Pro.local>
2021-01-21 23:08:12 -0500

    [CORRECTION IN NAME]

---
+++ 0x04-sandpiles/README.md
@@ -0,0 +1,1 @@
+<h1> SANDPILES </h1>
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+/* HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTYPES */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void printGrid(int grid[3][3]);
+
+#endif /* _SANDPILES_H_ */
