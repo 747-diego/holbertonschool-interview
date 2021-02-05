53e7a43d19b4b69272a97b0099b5bcc48835f6b6
Diego Tardio <diego@Diegos-MacBook-Pro.local>
2021-02-04 23:19:57 -0500

    [TASK 0. Linked list cycle]

--- 0x04-sandpiles.c/README.md
+++
@@ -1,1 +0,0 @@
-<h1> SANDPILES </h1>
---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: points to the begining of list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *first = list;
+	listint_t *second = list;
+
+	if (!list)
+		return (0);
+
+	while (first && second && second->next)
+	{
+		first = first->next;
+		second = second->next->next;
+
+		if (first == second)
+        {
+			return (1);
+        }
+	}
+
+	return (0);
+}
