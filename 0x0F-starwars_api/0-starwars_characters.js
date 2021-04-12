c6d739bea71dace61833124eb96fd72e94f64338
747-diego <725@holbertonschool.com>
2021-04-11 23:55:41 -0400

    [SEMI STANDARD APPROVED ]

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -7,14 +7,14 @@ const SWAPI = 'https://swapi-api.hbtn.io/api/films/';
 
 // REQUESTING STAR WARS API
 request(SWAPI + SWepisode, async function (err, res, body) {
-  if (err) return(console.log(err));
+  if (err) return (console.log(err));
 
   const SWAPIcharacters = JSON.parse(body).characters;
-  let characters
+  let characters;
   for (characters of SWAPIcharacters) {
     await new Promise(function (resolve, reject) {
       request(characters, function (err, res, body) {
-        if (err) return(console.log(err));
+        if (err) return (console.log(err));
         console.log(JSON.parse(body).name);
         resolve();
       });
