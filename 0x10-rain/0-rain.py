518d4d604d19346a17ba5015980d97d46abda9de
747-diego <725@holbertonschool.com>
2021-04-15 23:43:16 -0400

    [TASK 0. RAIN]

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -20,8 +20,8 @@ def rain(walls):
     endOfWaterContainer = len(walls) - 1
     if beforeRetained < endOfWaterContainer:
         afterRetained = afterRetained - currentAmountOfWAter
-        beforeRetained = endOfWaterContainer
         rainWater = beforeRetained
+        beforeRetained = endOfWaterContainer
         for water in range(endOfWaterContainer, rainWater, -1):
             if walls[water] >= walls[beforeRetained]:
                 beforeRetained = water
