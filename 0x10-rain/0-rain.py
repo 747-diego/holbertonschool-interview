c37587f76d41b1912f4fbc00399439a89d6f9859
747-diego <725@holbertonschool.com>
2021-04-15 23:30:44 -0400

    [TASK 0. Rain ]

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""TASK 0. Rain."""
+
+
+def rain(walls):
+    """Calculate how much water will be retained after it rains."""
+    if len(walls) is None:
+        return(0)
+    beforeRetained = 0
+    afterRetained = 0
+    currentAmountOfWAter = 0
+    for length in range(len(walls)):
+        if walls[length] >= walls[beforeRetained]:
+            beforeRetained = length
+        else:
+            CalculatedWater = walls[beforeRetained] - walls[length]
+            afterRetained = afterRetained + CalculatedWater
+            currentAmountOfWAter = currentAmountOfWAter + CalculatedWater
+    endOfWaterContainer = len(walls) - 1
+    if beforeRetained < endOfWaterContainer:
+        afterRetained = afterRetained - currentAmountOfWAter
+        beforeRetained = endOfWaterContainer
+        rainWater = beforeRetained
+        for water in range(endOfWaterContainer, rainWater, -1):
+            if walls[water] >= walls[beforeRetained]:
+                beforeRetained = water
+            else:
+                CalculatedWater = walls[beforeRetained] - walls[water]
+                afterRetained = afterRetained + CalculatedWater
+    totalAmountofRainWAter = afterRetained
+    return(totalAmountofRainWAter)
