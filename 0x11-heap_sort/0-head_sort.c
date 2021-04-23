03b27a75a787c4f08471c6cbfdfd190f4ebe602b
747-diego <725@holbertonschool.com>
2021-04-22 23:58:42 -0400

    [TEST]

---
+++ 0x11-heap_sort/0-head_sort.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+
+
+/**
+ * heap_sort - heap sort alorithm
+ * @array: the int array we are sorting
+ * @size: size of the array
+ * Return: void
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i = 0;
+    for (i = 1; i <10; i++) {
+        printf("%i", i);
+    }
+    return(0);
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,9 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <stddef.h>
+
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif /* _HEADER_H_ */
