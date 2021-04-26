468afc10cd6d16321f37f2f717c3ddc760793b5e
747-diego <725@holbertonschool.com>
2021-04-26 00:05:35 -0400

    [0. Heap sort ]

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,9 +1,12 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
 
+/* HEADER FILES */
 #include <stddef.h>
 
+/* PROTOTYPES */
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void sort(int *array, int size, int num, int finalSize);
 
 #endif /* _HEADER_H_ */
