6c913af70de7dc4cc7f129c9e93a45f4631659bd
747-diego <725@holbertonschool.com>
2021-05-13 23:56:21 -0400

    [TEST]

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extract root node from max binary heap
+ * @root: double pointer to root node of heap
+ * Return: value stored in root node or 0 or failure
+ */
+int heap_extract(heap_t **root)
+{
+    continue;
+}
