c033c9d7dbbb9d454136e8cb6545d3ef853bf67e
747-diego <725@holbertonschool.com>
2021-05-21 00:24:43 -0400

    [TASK 0.  Infinite Multiplication mandatory]

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,18 @@
+#ifndef _MULTIPLICATION_H_
+#define _MULTIPLICATION_H_
+
+/* HEADERS */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* PROTOTYPES */
+char *sumUp(char *numone, char *numtwo);
+char *test(char *string);
+int _putchar(char c);
+int confirmingArgs(char *firstArgmuent, char *secondArgument);
+void doubleCheck(char *argument);
+int cases(char numCharacter);
+int checkIfNULL(char *string);
+char *testTempo(char *argumentone, char *argumenttwo, int size);
+
+#endif
