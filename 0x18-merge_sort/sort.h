01a14f0362aa52ddf1e0bc3b9444329a4e715054
747-diego <725@holbertonschool.com>
2021-06-10 23:59:46 -0400

    [TASK 0. Merge Sort]

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,15 @@
+#ifndef _MERGE_
+#define _MERGE_
+
+/* HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* PROTOTYPES */
+int _putchar(char);
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void showArray(char *info, int *sort, size_t array)
+
+#endif
