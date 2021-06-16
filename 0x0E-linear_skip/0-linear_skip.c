d08f3820bd54a2d07754da62784dde22fe931194
747-diego <725@holbertonschool.com>
2021-06-16 18:53:29 -0400

    [TEST]

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -8,8 +8,6 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	char check[] = "Value checked at index [%ld] = [%d]\n";
-	char between[] = "Value found between indexes [%ld] and [%ld]\n";
 	skiplist_t *temp, *head = list, *node = list;
 	size_t index = 0, tempIndex = 0;
 	int totalValue = 0, size = 0;
@@ -21,10 +19,10 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		{
 			temp = node->express;
 			tempIndex = temp->index;
-			printf("%s",check, tempIndex, temp->n);
+			printf("Value checked at index [%ld] = [%d]\n", tempIndex, temp->n);
 			if (temp->n >= value)
 			{
-				printf("%s",between, index, tempIndex);
+				printf("Value found between indexes [%ld] and [%ld]\n", index, tempIndex);
 				totalValue = 1;
 			}
 			else
@@ -40,10 +38,10 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 					size += 1;
 				}
 				tempIndex = size;
-				printf("%s",between, index, tempIndex);
+				printf("Value found between indexes [%ld] and [%ld]\n", index, tempIndex);
 				totalValue = 1;
 			}
-			printf("%s",check, index, node->n);
+			printf("Value checked at index [%ld] = [%d]\n", index, node->n);
 			if (node->n == value)
 				return (node);
 			node = node->next;
