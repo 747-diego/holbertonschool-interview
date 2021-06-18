c7a39116082b738cbe2246ff862f6f0182623459
747-diego <725@holbertonschool.com>
2021-06-18 13:16:25 -0400

    [TASK 0. Binary Search]

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,7 +1,87 @@
-#include "algos.h"
+#include "search_algos.h"
 
-int advanced_binary(int *array, size_t size, int value) 
+/**
+ * showArray - displays array
+ * @currentNode: node index
+ * @newNode: node to lead the lenght of array
+ * @array: array to sort
+ * Return: void
+ */
+void showArray(int currentNode, int newNode, int *array)
 {
-    if (!array)
-        return(0);
+    int node;
+    char newLine[] = "\n";
+    char sep[] = ", ";
+    char messageDisplay[] = "Searching in array: ";
+    int nodePos = newNode + 1;
+
+	printf("%s", messageDisplay);
+	while (currentNode <= newNode)
+	{
+        node = array[currentNode++];
+		printf("%d", node);
+		if (currentNode != nodePos)
+			printf("%s", sep);
+	}
+	printf("%s", newLine);
+}
+
+/**
+ * valueSearch - searches for value in binary tree
+ * @currentNode: node index
+ * @newNode: node to lead the length of array
+ * @num: number inside node
+ * @array: array to sort
+ * @length: array length
+ * Return: num
+ */
+int valueSearch(int currentNode, int newNode, int num, int *array, int length)
+{
+    int nodeDividend = (newNode - currentNode) / 2;
+	int nodeSum = currentNode + nodeDividend;
+    int nodeIndex = array[currentNode];
+    int notPresentORnull = -1;
+
+	showArray(currentNode, newNode, array);
+	if (nodeIndex == num)
+    {
+		return (currentNode);
+    }
+
+
+	if (newNode - currentNode != 0)
+	{
+		if (num > array[nodeSum])
+        {
+			currentNode = nodeSum + 1;
+        }
+		else
+		{
+            newNode = nodeSum;
+        }
+
+		return (valueSearch(currentNode, newNode, num, array, length));
+	}
+
+	return (notPresentORnull);
+}
+
+
+/**
+ * advanced_binary - this function finds the first value of in the search.
+ * @array: array
+ * @size: length of the array
+ * @value: number inside of node
+ * Return: value
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL)
+    {
+		return (-1);
+    }
+	else 
+    {
+        return (valueSearch(0, size - 1, value, array, size));
+    }
 }
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SEARCH_ALGOS_H__
+#define __SEARCH_ALGOS_H__
+
+/* HEADER FILES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTYPES */
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* __SEARCH_ALGOS_H__ */
