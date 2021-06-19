45464a309db24120123e14e11f7f5accbdd71ca0
747-diego <725@holbertonschool.com>
2021-06-19 04:51:52 -0400

    [MERGE]

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -9,7 +9,8 @@
 void displayNodes(int array[], size_t headNode, size_t tailNode);
 void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void nodeSwap(int array[], size_t headNode, size_t bodyNode, size_t nodeTail, int currentArray[]);
+void nodeSwap(int array[], size_t headNode, size_t bodyNode,
+	size_t nodeTail, int currentArray[]);
 void split(int currentNode[], size_t headNode, size_t tailNode, int array[]);
 
 #endif
