84acfe5cbbe0aae6367366eb95f222358e6e7d89
747-diego <725@holbertonschool.com>
2021-06-19 02:54:33 -0400

    [TESTING]

--- 0x17-find_the_loop/lists.h
+++ 0x17-find_the_loop/lists.h
@@ -23,7 +23,6 @@ typedef struct listint_s
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listint_t findLoop(listint_t *numOne, listint_t *numTwo);
 listint_t *find_listint_loop(listint_t *head);
 
 
