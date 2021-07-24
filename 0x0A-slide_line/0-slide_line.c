df588c9d06ca3bbccc021f8b5d69bc3d09dc662b
747-diego <725@holbertonschool.com>
2021-07-24 00:32:25 -0400

    [TESTING]

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -64,19 +64,19 @@ void shiftRight(int *line, size_t size)
 				line[size - numOne] = line[size - numTwo];
 				line[size - numTwo] = 0;
 				numOne -= 1;
-				break;
+				continue;
 			}
 			if ( line[size - numOne] != 0 && line[size - numOne] == line[size - numTwo] )
 			{
 				line[size - numOne] = line[size - numOne] * 2;
 				line[size - numTwo] = 0;
-				break;
+				continue;
 			}
 			else if ( line[size - (numOne++)] == 0 && line[size - numTwo] != 0 )
 			{
 				line[size - (numOne++)] = line[size - numTwo];
 				line[size - numTwo] = 0;
-				break;
+				continue;
 			}
 			numTwo++;
 		}
