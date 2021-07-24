a56ab1c1ba389e95d13d76962583439e97a9cdc8
747-diego <725@holbertonschool.com>
2021-07-23 23:54:59 -0400

    [TESTING]

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
               "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
     FileSize = 0
+    counter = 0
 
     def getLog(line):
         """Parse the logs and gets status code."""
