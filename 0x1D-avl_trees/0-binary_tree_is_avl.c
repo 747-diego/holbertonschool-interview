4867f69f414b0c6e1de829b981b4d653e1d4a03e
747-diego <725@holbertonschool.com>
2021-07-26 13:22:32 -0400

    [TESTING]

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,63 +1,65 @@
 #include "binary_trees.h"
 
 /**
- * validTree - tree to determine whethere it is valid
- * @head: first ptr
- * @tailnode: last ptr
- * @lengthOfTree: size of ptr's
- * @beginningOfTree: first ptr
- * @sizeOfTree: size of all ptr's
- * Return: tree
+ * validTree - checking binary tree
+ * @root: beginning of tree
+ * @endOfTree: end binary tree
+ * @treeLength: length of binary tree
+ * @node: node
+ * @size: size of tree
+ * Return: valid or non vaild
  */
-int validTree(binary_tree_t *head, int *tailnode, int *lengthOfTree, int *beginningOfTree, int sizeOfTree)
+int validTree(binary_tree_t *root, int *endOfTree, int *treeLength, int *node, int size)
 {
 	int tree = 1;
 
-    if (tree == 0)
+	if (root->left)
+    {
+		tree = validTree(root->left, endOfTree, treeLength, node, size + 1);
+    }
+	if (tree == 0)
     {
 		return (0);
     }
-	if (head->left)
+	if (size > *treeLength)
     {
-		tree = validTree(node->left, tailnode, lengthOfTree, beginningOfTree, sizeOfTree + 1);
+		*treeLength = size;
     }
-	if (sizeOfTree > *lengthOfTree)
-		*lengthOfTree = sizeOfTree;
-	if (!(head->left && head->right))
+
+	if (!(root->left && root->right))
     {
-		if (*beginningOfTree == -2147483648 || sizeOfTree < *beginningOfTree)
+		if (*node == -2147483648 || size < *node)
         {
-			*beginningOfTree = sizeOfTree;
+			*node = size;
         }
     }
-	if (*lengthOfTree - *beginningOfTree > 1)
-    {
+
+	if (*treeLength - *node > 1)
 		return (0);
-    }
-	if (*tailnode >= head->n)
-    {
+	if (*endOfTree >= root->n)
 		return (0);
+	*endOfTree = root->n;
+	if (root->right)
+    {
+		tree = validTree(root->right, endOfTree, treeLength, node, size + 1);
     }
-	*tailnode = head->n;
-	if (head->right)
-		tree = validTree(head->right, tailnode, lengthOfTree, beginningOfTree, sizeOfTree + 1);
 	if (tree == 0)
 		return (0);
 	return (1);
 }
 
 /**
- * binary_tree_is_avl - tree is valid
- * @tree: tree
- * Return: valid or non valid
+ * binary_tree_is_avl - checking if valid
+ * @tree: head node
+ * Return: check if valid or non valid
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    int tailnode = -2147483648;
-	int treelength = -2147483648;
-	int headnode = -2147483648;
+    int node = -2147483648;
+	int end = -2147483648;
+	int length = -2147483648;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	return (validTree((binary_tree_t *)tree, &tailnode, &treelength, &headnode, 0));
+	return (validTree((binary_tree_t *)tree, &end, &length, &node, 0));
 }
