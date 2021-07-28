7d97b3ce9643de17a5c768378259e4c91314c0be
747-diego <725@holbertonschool.com>
2021-07-28 13:13:23 -0400

    [TASK 0. WILD COMPARE]

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+
+/**
+ * wildcmp - wild compare
+ * @s1: string
+ * @s2: special character
+ * Return: identical string
+ */
+int wildcmp(char *s1, char *s2)
+{
+	char *stringOne = s1;
+	char *stringTwo = s2;
+	char specialCharacter = '*';
+	int compare;
+	
+
+	if (*stringOne == '\0' && *stringTwo == '\0')
+		return (1);
+	if (*stringOne == *stringTwo)
+	{
+		compare = wildcmp(stringOne += 1, stringTwo += 1);
+		return (compare);
+	}
+	if (*stringTwo == specialCharacter)
+	{
+		if (*s1 != '\0')
+		return (wildcmp(stringOne, stringTwo + 1) || wildcmp(stringOne + 1, stringTwo));
+	}
+	return (0);
+}
