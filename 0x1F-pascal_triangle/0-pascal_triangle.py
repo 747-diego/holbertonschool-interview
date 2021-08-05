5b40642abf1963c819d2bee92f4b20a80b3c4c39
747-diego <725@holbertonschool.com>
2021-08-05 00:47:40 -0400

    [TASK 0. PASCAL TRIANGLE]

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""TASK 0. Pascal's Triangle."""
+
+
+def pascal_triangle(n):
+    """Create a function that returns a list of lists of Pascal’s triangles."""
+    triNum = n
+    lists = []
+    for list in range(triNum):
+        angle = []
+        for integer in range(list + 1):
+            if integer == 0:
+                angle.append(1)
+                continue
+            if integer == list:
+                angle.append(1)
+                continue 
+            down = lists[list - 1][integer]
+            downOne = lists[list - 1][integer - 1]
+            angle.append(downOne + down)
+        lists.append(angle)
+    return (lists)
